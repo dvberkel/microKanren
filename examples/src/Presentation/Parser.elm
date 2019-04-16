3f6b1ac78b8d5a214e3c66d6b90a015444c46324
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-04-16 20:11:57 +0200

    Parse Markdown into a presentation

--- examples/src/Presentation/Parser.elm
+++ examples/src/Presentation/Parser.elm
@@ -6,26 +6,51 @@ import Presentation.Kernel as Presentation exposing (Presentation, Slide(..))
 type ParseError
     = NoSlides
 
+
 parse : String -> Result ParseError Presentation
 parse input =
     let
         toPresentation =
             Presentation.fromList
-            >> Result.fromMaybe NoSlides
+                >> Result.fromMaybe NoSlides
     in
     input
         |> parseSlides
-        |> Result.andThen toPresentation 
+        |> Result.andThen toPresentation
+
 
 parseSlides : String -> Result ParseError (List Slide)
 parseSlides input =
-   input
+    input
         |> String.split "---\n"
         |> parseMultipleSlides
+
 
 parseMultipleSlides : List String -> Result ParseError (List Slide)
 parseMultipleSlides inputs =
     inputs
-        |> List.map (\_ -> Blank)
-        |> Ok
- 
+        |> List.map parseSlide
+        |> gather
+
+
+parseSlide : String -> Result ParseError Slide
+parseSlide input =
+    Ok <| Markdown input
+
+
+gather : List (Result e a) -> Result e (List a)
+gather =
+    accumulatedGather []
+
+
+accumulatedGather : List a -> List (Result e a) -> Result e (List a)
+accumulatedGather acc xs =
+    case xs of
+        [] ->
+            Ok <| List.reverse acc
+
+        (Err e) :: _ ->
+            Err e
+
+        (Ok x) :: tail ->
+            accumulatedGather (x :: acc) tail
--- examples/tests/ParserTest.elm
+++ examples/tests/ParserTest.elm
@@ -1,13 +1,14 @@
-module ParserTest exposing (..)
+module ParserTest exposing (exampleInput, suite)
 
 import Expect exposing (Expectation)
-import Test exposing (..)
-import Presentation.Kernel exposing (slideCount)
+import Presentation.Kernel as Presentation exposing (Slide(..), emptyPresentation, slideCount)
 import Presentation.Parser exposing (parse)
+import Test exposing (..)
+
 
 exampleInput : String
-exampleInput = """
-# Presentation
+exampleInput =
+    """# Presentation
 ## An Elm module
 
 ---
@@ -18,20 +19,36 @@ exampleInput = """
 
 """
 
+
 suite : Test
 suite =
-    describe "parse" [
-        test "slides are delimited by \"---\"" <|
-        \_ ->
-        let
-            actual =
-                exampleInput
-                |> parse
-                |> Result.map slideCount
+    describe "parse"
+        [ test "slides are delimited by \"---\"" <|
+            \_ ->
+                let
+                    actual =
+                        exampleInput
+                            |> parse
+                            |> Result.map slideCount
 
-            expected =
-                Ok 3
-        in  
-            Expect.equal actual expected
-        
-    ]
+                    expected =
+                        Ok 3
+                in
+                Expect.equal actual expected
+        , test "first slide is Markdown" <|
+            \_ ->
+                let
+                    actual =
+                        exampleInput
+                            |> parse
+                            |> Result.toMaybe 
+
+                    expected =
+                        [ Markdown "# Presentation\n## An Elm module\n\n"
+                        , Markdown "# Yet an other slide\n\n"
+                        , Markdown "# Last slide\n\n"
+                        ]
+                            |> Presentation.fromList
+                in
+                Expect.equal actual expected
+        ]
